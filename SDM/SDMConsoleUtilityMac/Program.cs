b75efad4f0c7827ff12f7b9a29a5562d2bd4e409
Unknown <ryan_s.kennedy@mail.ru>
2018-08-23 23:25:56 +0300

    Add return help + return version + create dictionary with args

--- SDM/SDMConsoleUtilityMac/Program.cs
+++ SDM/SDMConsoleUtilityMac/Program.cs
@@ -1,12 +1,127 @@
 ﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
 
 namespace SDMConsoleUtilityMac
 {
     class Program
     {
+        public static Arguments[] argsMass;
+        public static string baseDir = "";
+        public static Dictionary<string, string> argsArray = new Dictionary<string, string>();
+
+
+        public struct Arguments 
+        {
+            public string arg;
+            public string value;
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // получение пути до базовой директории где расположено приложение
+            //============================================= 
+            System.Reflection.Assembly a = System.Reflection.Assembly.GetEntryAssembly();
+            baseDir = System.IO.Path.GetDirectoryName(a.Location);
+            //=============================================
+
+            SDMCore.DownloadClass myClass = new SDMCore.DownloadClass();
+
+            argsMass = new Arguments[args.Length];
+            int i = 0;
+
+            // проверка массива переданных приложению аргументов на наличие команд для вывода версии или справки, а также на корректность вводимых аргументов
+            //=============================================
+            foreach(string el in args) 
+            {
+                if (String.IsNullOrEmpty(el)) 
+                {
+                    break;
+                }
+
+                if (el.StartsWith('-') && el.Contains(":"))
+                {
+                    string[] tmpArg = el.Split(':', 2);
+                    argsMass[i].arg = tmpArg[0].Substring(1);
+                    argsMass[i].value = tmpArg[1];
+                    i++;
+                } else if (el.StartsWith('-') && (el.Substring(1,el.Length-1).StartsWith('h') || el.Contains("help"))) {
+                    Console.WriteLine(SDMCore.DownloadClass.ReturnHelp());
+                    return;
+                } else if (el.StartsWith('-') && (el.Substring(1, el.Length - 1).StartsWith('v') || el.Contains("version"))){
+                    Console.WriteLine("Version SDMCore.dll: " + SDMCore.DownloadClass.GetVersion());
+                    Console.WriteLine("Version SDMConsoleUtility: " + typeof(Program).GetTypeInfo().Assembly.GetName().Version.ToString());
+                    return;
+                } else {
+                    Console.WriteLine("You using incorrect arguments: " + el + Environment.NewLine + "//===========================" + Environment.NewLine);
+                    return;
+                }
+            }
+            //=============================================
+
+            // проверка массива переданных приложению аргументов и их значений на валидность и заполнение коллекцию ключей и значений
+            //=============================================
+            foreach (Arguments el in argsMass) 
+            {
+                switch (el.arg)
+                {
+                    case "u":
+                    case "url":
+                        argsArray.Add(el.arg,el.value);
+                        break;
+
+                    case "l":
+                    case "login":
+                        argsArray.Add(el.arg, el.value);
+                        break;
+
+                    case "p":
+                    case "password":
+                        argsArray.Add(el.arg, el.value);
+                        break;
+
+                    case "f":
+                    case "folder":
+                        argsArray.Add(el.arg, el.value);
+                        break;
+
+                    case "r":
+                    case "resume":
+                        if (el.value != "0" && el.value != "1") 
+                        {
+                            argsArray.Add(el.arg, "0");
+                        } else {
+                            argsArray.Add(el.arg, el.value);
+                        } 
+                        break;
+
+                    case "t":
+                    case "thread":
+                        if (0 < Convert.ToInt32(el.value) && Convert.ToInt32(el.value) < 6)
+                        {
+                            argsArray.Add(el.arg, el.value);
+                        }
+                        else
+                        {
+                            argsArray.Add(el.arg, "1");
+                        }
+                        break;
+
+                    case "a":
+                    case "adapter":
+                        argsArray.Add(el.arg, el.value);
+                        break;
+
+                    default:
+                        Console.WriteLine("You using incorrect arguments: " + el + Environment.NewLine + "//===========================" + Environment.NewLine);
+                        return;
+                }
+            }
+            //=============================================
+
+            Console.WriteLine("//===========================");
+            Console.WriteLine(SDMCore.DownloadClass.SayHi());
         }
     }
 }
---
+++ SDM/SDMCore/DownloadClass.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Reflection;
+
+namespace SDMCore
+{
+    public class DownloadClass
+    {
+        public DownloadClass()
+        {
+            
+        }
+
+        public static string SayHi()
+        {
+            return "Hi Men!";
+        }
+
+        public static string ReturnHelp()
+        {
+            string help = "This is Help for SDMCore DLL." + Environment.NewLine +
+                          "You can use next arguments:" + Environment.NewLine +
+                          "\"-h\" or \"-help\" - for display this Help information." + Environment.NewLine +
+                          "\"-v\" or \"-version\" - for display version of SDMUtility" + Environment.NewLine +
+                          "\"-u\" or \"-url\" - for set download link." + Environment.NewLine +
+                          "\"-l\" or \"-login\" - for set login for FTP connection." + Environment.NewLine +
+                          "\"-p\" or \"-password\" - for set password for FTP connection." + Environment.NewLine +
+                          "\"-f\" or \"-folder\" - for set folder where should save content (by default using folder where located SDMUtility)." + Environment.NewLine +
+                          "\"-r\" or \"-resume\" - for set on/off resuming downloading proccess if needed (on=1, off=0, by Default=0)." + Environment.NewLine +
+                          "\"-t\" or \"-thread\" - for set number of thread for downloading (min=1, max=5, by Default=1)." + Environment.NewLine +
+                          "\"-a\" or \"-adapter\" - for set which network adapter should using for downloading." + Environment.NewLine + 
+                          Environment.NewLine + "Example of calling:" + Environment.NewLine +
+                          "...\\SDMConsoleUtilityMac.dll -u:http:\\\\testServer:8080\\1.mp4 -f:\"C:\\tmp\\\" -r:1 -t:3" + Environment.NewLine;
+            
+
+            return help;
+        }
+
+        public string GetHttp() 
+        {
+
+
+            return "http_result";
+        }
+
+        public string GetHttps()
+        {
+
+
+            return "https_result";
+        }
+
+        public string GetFtp()
+        {
+
+
+            return "ftp_result";
+        }
+
+        public static string GetVersion()
+        {
+            return typeof(DownloadClass).GetTypeInfo().Assembly.GetName().Version.ToString();
+        }
+    }
+}
