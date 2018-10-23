3cbd3297e33729326abbed4009efdaae49f48436
Unknown <ryan_s.kennedy@mail.ru>
2018-10-23 20:40:49 +0300

    Add get HTTP request

--- SDM/SDMCore/HttpRequestClass.cs
+++ SDM/SDMCore/HttpRequestClass.cs
@@ -1,10 +1,25 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
 namespace SDMCore
 {
     public class HttpRequestClass
     {
         public HttpRequestClass()
         {
+        }
+
+        public static HttpResponseMessage GetRequest (string requestUrl) 
+        {
+            HttpClient request = new HttpClient();
+            HttpResponseMessage response = null;
+            response = request.GetAsync(requestUrl).Result;
+
+            return response;
         }
     }
 }
