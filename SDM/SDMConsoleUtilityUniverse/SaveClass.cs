43b666dff066f5c0bfac2fcb92f0929935978b1b
Unknown <ryan_s.kennedy@mail.ru>
2018-10-31 11:04:58 +0300

    Merge in one project

---
+++ SDM/SDMConsoleUtilityUniverse/SaveClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace SDMCore
+{
+    public class SaveClass
+    {
+        public static string baseDir = "";
+
+        // конструктор класса
+        //=============================================
+        public SaveClass()
+        {
+            // получение пути до базовой директории где расположено приложение
+            //============================================= 
+            baseDir = SDMCore.InfoClass.GetBaseDir();
+            //=============================================
+        }
+        //=============================================
+
+        // функция сохранения данных в файл 
+        //=============================================
+        public static bool SaveTo(string fileName, string filePath, byte[] fileData)
+        {
+            if (!File.Exists(filePath + Path.DirectorySeparatorChar + fileName)) File.Create(filePath + Path.DirectorySeparatorChar + fileName);
+            {
+                StreamWriter output = new StreamWriter(filePath + Path.DirectorySeparatorChar + fileName);
+                try
+                {
+                    output.Write(fileData);
+                    output.Close();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
+        //=============================================
+    }
+}
